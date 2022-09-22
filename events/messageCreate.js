f43abf359bcb6dc427026992b40639541c1beb08
samuelcolles <samueltaziocolles@hotmail.com>
2022-09-21 23:48:09 -0700

    save before implementing sequelize

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,14 +11,18 @@ module.exports = {
 		// only reply to messages in a certain channel and ignore messages that are sent by this bot
 		const senderID = message.author.id
 		if (message.channelId !== process.env.TEXT_CHANNEL_ID || senderID === message.client.user.id) return
-		updateMessageCounter(message.client)
 
 		const channel = message.client.channels.cache.get(process.env.TEXT_CHANNEL_ID)
+
 		if (senderID === process.env.TARGET_USER_ID) {
+			/* Handle messages coming from the target*/
+			updateMessageCounter(message.client)
 			const mocks = message.client.config.responses.mocks
 			if (Math.random() < chanceToMock) await channel.send(`${whackyCase(message.content)}${mocks[parseInt(Math.random() * mocks.length)]}`)
 			return
+
 		} else if (Math.random() < chanceToPraise) {
+			/* Handle messages coming from others in the same channel */
 			const praises = message.client.config.responses.praises
 			await channel.send(`${praises[parseInt(Math.random() * praises.length)]}, <@${senderID}>!`)
 			return
--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs')
 const path = require('node:path')
 
+
 module.exports = {
 	whackyCase(input) {
 		let res = ''
@@ -12,8 +13,20 @@ module.exports = {
 	updateMessageCounter(client) {
 
 		client.config.messageCounter++
+		if (client.config.messageCounter === client.config.threasholds.phase2) {
+			console.log('Phase 2 activated!')
+			client.config.phase = 2
+		}
+		if (client.config.messageCounter === client.config.threasholds.phase3) {
+			console.log('Phase 3 activated!')
+			client.config.phase = 3
+		}
+		if (client.config.messageCounter === client.config.threasholds.retire) {
+			console.log('Final Phase complete, retiring robot.')
+			client.config.phase = 4
+		}
+		// save the message counter to the config
 		console.log(client.config.messageCounter)
-		console.log(path.join(__dirname, '../customConfig.json'))
 		fs.writeFileSync(path.join(__dirname, '../customConfig.json'), JSON.stringify(client.config, null, 4))
 	},
 
